5d2a28323574357b93d01a422ae5207ebb7670c7
Will Eaton <wseaton93@gmail.com>
2016-11-02 01:09:22 -0400

    added match log at index with js datatables

--- run.py
+++ run.py
@@ -4,12 +4,20 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 from sqlalchemy import Column, Integer, Text, create_engine, MetaData
 import pandas as pd
+import time
 
-app = Flask(__name__, static_url_path='')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pong.db'
+def create_app():
+    app = Flask(__name__, static_url_path='')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pong.db'
+    Bootstrap(app)
+
+    return app
+
+app = create_app()
 db = SQLAlchemy(app)
 
 class Game(db.Model):
@@ -37,22 +45,22 @@ metadata = MetaData(bind=engine)
 @app.route('/', methods=['GET', 'POST'])
 def matches():
     df = pd.read_sql('select * from game', con=engine)
-    df = df.drop('id', axis=1)
-
-    return render_template('index.html', match_data=df.to_html())
+    df = df.to_dict('records')
+    return render_template('index.html', games=df)
 
 @app.route('/record_match', methods=['GET', 'POST'])
 def record_match():
     form = MatchForm(csrf_enabled=False)
     if form.validate_on_submit():
+        record = Game(player_a=form.player_a.data, player_b=form.player_b.data,
+                      score_a=form.score_a.data, score_b=form.score_b.data,
+                      timestamp=time.time())
+        db.session.add(record)
+        db.session.commit()
+        return 'success! your match was uploaded.'
 
-
-
-        return redirect('success!')
     return render_template('addmatch.html', form=form)
 
 
-app.debug = True
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
