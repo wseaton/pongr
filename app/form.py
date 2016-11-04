14fc26433a03dbd82292493ff9c0785790caff20
Will Eaton <wseaton93@gmail.com>
2016-11-03 20:55:03 -0400

    fix form bug

--- app/form.py
+++ app/form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class MatchForm(FlaskForm):
-    player_a = StringField('player_a', validators=[DataRequired(), Length(max=20)])
-    player_b = StringField('player_b', validators=[DataRequired(), Length(max=20)])
-    score_a = IntegerField('score_a', validators=[DataRequired(), Length(max=2)])
-    score_b = IntegerField('score_b', validators=[DataRequired(), Length(max=2)])
+    player_a = StringField('player_a', validators=[DataRequired()])
+    player_b = StringField('player_b', validators=[DataRequired()])
+    score_a = IntegerField('score_a', validators=[DataRequired()])
+    score_b = IntegerField('score_b', validators=[DataRequired()])
